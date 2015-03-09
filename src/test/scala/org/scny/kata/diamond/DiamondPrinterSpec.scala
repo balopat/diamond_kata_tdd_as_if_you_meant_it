7a0b96d8c85d1adff196febe0690e452035ce9d1
balopat <balopat@balopat.com>
2015-03-09 18:54:15 -0400

    DELETE ==> this is where I restarted the exercise, there was no real duplication to remove

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -6,20 +6,44 @@ import org.scalatest.{Matchers, FlatSpec}
 class DiamondPrinterSpec extends FlatSpec with Matchers {
 
   "When I print an A in diamond the printer" should "give back two lines of A" in {
-    diamondLines('A') should be(List(
+    val input = 'A'
+
+    val diamondLines = List(
+      "A",
+      "A")
+
+
+    diamondLines should be(List(
       "A",
       "A"))
   }
 
   "When I print a B in diamond the printer" should "give back two lines of A, and a B in the middle" in {
-    diamondLines('B') should be(List(
+    val input = 'B'
+
+    val diamondLines = List(
+      " A",
+      "B B",
+      " A")
+
+    diamondLines should be(List(
       " A",
       "B B",
       " A"))
   }
 
   "When I print a C in diamond the printer" should "give back the 4 diamond lines up until C" in {
-    diamondLines('C') should be(List(
+    val input = 'C'
+
+    val diamondLines = List(
+      "  A",
+      " B B",
+      "C   C",
+      " B B",
+      "  A"
+    )
+
+    diamondLines should be(List(
       "  A",
       " B B",
       "C   C",
@@ -29,26 +53,6 @@ class DiamondPrinterSpec extends FlatSpec with Matchers {
   }
 
 
-  def diamondLines(inputCharacter: Char): List[String] = {
 
-    val diamondLines =  inputCharacter match {
-      case 'A' => List(
-        "A",
-        "A")
-      case 'B' => List(
-        " A",
-        "B B",
-        " A")
-      case 'C' => List(
-        "  A",
-        " B B",
-        "C   C",
-        " B B",
-        "  A"
-      )
-    }
-
-    diamondLines
-  }
 
 }
