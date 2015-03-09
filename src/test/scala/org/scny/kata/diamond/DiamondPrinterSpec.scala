8bdcf8a3c93177092846876de67930ea9605da4c
balopat <balopat@balopat.com>
2015-03-09 18:47:32 -0400

    the test case for C is there too, but obviously it's not sustainable to continue down this path, it won't scale, so I need to make the design better now

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -6,19 +6,46 @@ import org.scalatest.{Matchers, FlatSpec}
 class DiamondPrinterSpec extends FlatSpec with Matchers {
 
   "When I print an A in diamond the printer" should "give back two lines of A" in {
-    diamondLines('A') should be(List("A","A"))
+    diamondLines('A') should be(List(
+      "A",
+      "A"))
   }
 
   "When I print a B in diamond the printer" should "give back two lines of A, and a B in the middle" in {
-    diamondLines('B') should be(List(" A ","B"," A "))
+    diamondLines('B') should be(List(
+      " A",
+      "B B",
+      " A"))
+  }
+
+  "When I print a C in diamond the printer" should "give back the 4 diamond lines up until C" in {
+    diamondLines('C') should be(List(
+      "  A",
+      " B B",
+      "C   C",
+      " B B",
+      "  A"
+    ))
   }
 
 
   def diamondLines(inputCharacter: Char): List[String] = {
 
     val diamondLines =  inputCharacter match {
-      case 'A' => List("A", "A")
-      case 'B' => List(" A ","B"," A ")
+      case 'A' => List(
+        "A",
+        "A")
+      case 'B' => List(
+        " A",
+        "B B",
+        " A")
+      case 'C' => List(
+        "  A",
+        " B B",
+        "C   C",
+        " B B",
+        "  A"
+      )
     }
 
     diamondLines
