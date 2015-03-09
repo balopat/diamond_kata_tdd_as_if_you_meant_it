44d6f09f9bf9fe4218a34ae2a32834ba96f1a637
balopat <balopat@balopat.com>
2015-03-09 18:23:11 -0400

    B passed. Still before refactoring

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -17,7 +17,7 @@ class DiamondPrinterSpec extends FlatSpec with Matchers {
   "When I print a B in diamond the printer" should "give back two lines of A, and a B in the middle" in {
     val inputCharacter = "B"
 
-    val outputLines = List("A","A")
+    val outputLines = List(" A ","B"," A ")
 
     outputLines should be(List(" A ","B"," A "))
   }
