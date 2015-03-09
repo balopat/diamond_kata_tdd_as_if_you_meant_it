7a0814f93a7bd31fa4253518d3ffa84828116c93
balopat <balopat@balopat.com>
2015-03-09 18:31:30 -0400

    Refactoring halfway through: extracting method diamondLines to remove the duplication of 'diamond lines for a given character' logic

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -6,21 +6,23 @@ import org.scalatest.{Matchers, FlatSpec}
 class DiamondPrinterSpec extends FlatSpec with Matchers {
 
   "When I print an A in diamond the printer" should "give back two lines of A" in {
-    val inputCharacter = "A"
-
-    val outputLines = List("A","A")
-
-    outputLines should be(List("A","A"))
+    diamondLines('A') should be(List("A","A"))
   }
+
 
 
   "When I print a B in diamond the printer" should "give back two lines of A, and a B in the middle" in {
-    val inputCharacter = "B"
+    val inputCharacter = 'B'
 
-    val outputLines = List(" A ","B"," A ")
+    val diamondLines = List(" A ","B"," A ")
 
-    outputLines should be(List(" A ","B"," A "))
+    diamondLines should be(List(" A ","B"," A "))
   }
 
+
+  def diamondLines(inputCharacter: Character): List[String] = {
+    val diamondLines = List("A", "A")
+    diamondLines
+  }
 
 }
