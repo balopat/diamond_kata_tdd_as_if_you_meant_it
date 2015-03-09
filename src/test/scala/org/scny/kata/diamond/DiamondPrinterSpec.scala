02861af9ef41790c36e82610be9267c8c32bdb64
balopat <balopat@balopat.com>
2015-03-09 18:34:07 -0400

    refactoring done with silly switch statement

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -9,19 +9,18 @@ class DiamondPrinterSpec extends FlatSpec with Matchers {
     diamondLines('A') should be(List("A","A"))
   }
 
-
-
   "When I print a B in diamond the printer" should "give back two lines of A, and a B in the middle" in {
-    val inputCharacter = 'B'
-
-    val diamondLines = List(" A ","B"," A ")
-
-    diamondLines should be(List(" A ","B"," A "))
+    diamondLines('B') should be(List(" A ","B"," A "))
   }
 
 
-  def diamondLines(inputCharacter: Character): List[String] = {
-    val diamondLines = List("A", "A")
+  def diamondLines(inputCharacter: Char): List[String] = {
+
+    val diamondLines =  inputCharacter match {
+      case 'A' => List("A", "A")
+      case 'B' => List(" A ","B"," A ")
+    }
+
     diamondLines
   }
 
