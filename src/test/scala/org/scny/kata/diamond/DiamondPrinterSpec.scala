09b42c89fef8ed743130e18574d73de6239afcae
balopat <balopat@balopat.com>
2015-03-09 20:28:08 -0400

    [RED] tooo muchscala! reverting to a smaller use case...

--- src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
+++ src/test/scala/org/scny/kata/diamond/DiamondPrinterSpec.scala
@@ -27,29 +27,50 @@ class DiamondPrinterSpec extends FlatSpec with Matchers {
   }
 
   "For C, the diamond printer" should "have two B-s printed with one space separated in the second line + one indentation" in {
-    printDiamond('C').indexWhere(_.contains(" B B")) should be(1)
+    printDiamond('C').count(_.contains(" B B")) should be(2)
   }
 
   "For C, the diamond printer" should "have two double indented A-s" in {
     printDiamond('C').count(_ == "  A" ) should be(2)
   }
 
+
+  "For C, the diamond printer" should "have two C-s in the third row with 3 spaces separation" in {
+    printDiamond('C').indexWhere(_ == "C   C" ) should be(2)
+  }
+
+
   def indentLines(previousCharacter: Char): List[String] = {
-    printDiamond(previousCharacter).map(" " + _)
+    indentLines(printDiamond(previousCharacter))
+  }
+
+  def indentLines(printedLines: List[String]): List[String] = {
+    printedLines.map(" " + _)
   }
 
   def printDiamond(char:Char): List[String] = {
     char match {
       case 'A' =>   List(char.toString)
       case 'B' =>
-            val lines = indentLines('A')
-            lines ++ List(char + " " + char) ++ lines
-      case 'C' => indentLines('B')
+            indentLines(newLine('A')) ++ newLine('B') ++ indentLines(newLine('A'))
+      case 'C' =>
+        val sorroundWithPrevious: List[String] =
+          indentLines(newLine('B')) ++
+            List("C   C") ++
+          indentLines(newLine('B'))
+
+        indentLines(indentLines('A')) ++
+          sorroundWithPrevious ++
+          indentLines(indentLines('A'))
     }
 
 
   }
 
 
-
+  def newLine(char:Char): List[String] = {
+    val numberOfSpaces = char - 'A'
+    val line: String = char + " " + char
+    List(line)
+  }
 }
